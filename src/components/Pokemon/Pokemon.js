6068d54bd3cde07fcf186722aea9769a74ca75cf
leo07vasp <leo07vasp@gmail.com>
2021-02-28 19:23:28 -0300

    type pokemon

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -8,7 +8,7 @@ const Pokemon = (props) => {
       <img src={`${process.env.PUBLIC_URL}/pokemons/name/${props.pokemon.name}.png`} />
       <h4>{props.pokemon.name}</h4>
       <ul>
-        {Array.isArray(props.pokemon.types) && props.pokemon.types.length > 1 && props.pokemon.types.map(type => {
+        {Array.isArray(props.pokemon.types) && props.pokemon.types.map(type => {
             return <li>{type.type.name}</li>
         })}
       </ul>
